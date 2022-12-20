61ef5bc45c3774b2716c1f68413af04101e0e437
Nikos Dim <ndimitrakopoulos@maseurope.com>
2022-12-20 10:01:05 +0200

    added all days

---
+++ day-10/b.js
@@ -0,0 +1,40 @@
+//https://adventofcode.com/2022/day/4
+
+const fs = require("fs");
+
+const fileContent = fs.readFileSync("data.txt", "utf-8");
+const fileLines = fileContent.split(/\r?\n/);
+
+const isOverlapping = (fromA, toA, fromB, toB) => {
+  if (toA >= fromB && toA <= toB) {
+    return true;
+  }
+
+  if (fromA <= fromB && toA >= toB) {
+    return true;
+  }
+
+  if (fromA >= fromB && fromA <= toB) {
+    return true;
+  }
+
+  return false;
+};
+
+let sum = 0;
+fileLines.forEach((line) => {
+  const [a, b] = line.split(",");
+
+  const [fromA, toA] = a.split("-");
+  const [fromB, toB] = b.split("-");
+
+  const value = isOverlapping(
+    Number(fromA),
+    Number(toA),
+    Number(fromB),
+    Number(toB)
+  );
+  sum += Number(value);
+});
+
+console.log(sum);
